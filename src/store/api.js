8518e3b5a504fe9d6327a0844dcb2db903326072
klimalik <klimamalik14@gmail.com>
2024-04-04 22:05:12 +0700

    edit endpoint api

--- src/store/api.js
+++ src/store/api.js
@@ -1,6 +1,7 @@
 import { getCookies } from "./utils";
 
-const endpoint = 'http://localhost:3000'
+// const endpoint = 'http://localhost:3000'
+const endpoint = 'https://jqg00d9f-3000.asse.devtunnels.ms'
 
 // + Auth 
 export const loginAkun = async (data) => {
