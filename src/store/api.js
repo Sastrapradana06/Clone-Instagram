7cb3572227edd609a47dbcfc0c36edfa0d4b2f18
klimalik <klimamalik14@gmail.com>
2024-04-13 23:21:00 +0700

    fitur: show loved for my postingan

--- src/store/api.js
+++ src/store/api.js
@@ -1,8 +1,8 @@
 import { deleteImage } from "./db";
 import { getCookies } from "./utils";
 
-const endpoint = 'http://localhost:3000'
-// const endpoint = 'https://jqg00d9f-3000.asse.devtunnels.ms'
+// const endpoint = 'http://localhost:3000'
+const endpoint = 'https://jqg00d9f-3000.asse.devtunnels.ms'
 
 
 // + Auth 
